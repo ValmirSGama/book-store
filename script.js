7b3d9ab72c57c6a5f1889e6c329b9074a4f49d12
Valmir Sales <valmir_salesgama@hotmail.com>
2024-10-23 23:51:32 -0300

    Adição da seção Featured Books com carrossel e estilização

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-searchForm = document.querySelector('.search-form');
+searchForm = document.querySelector('.search-form')
 
 document.querySelector('#search-btn').onclick = () => {
-    searchForm.classList.toggle('active');
+    searchForm.classList.toggle('active')
 }
 
 window.onscroll = () => {
-    searchForm.classList.remove('active');
+    searchForm.classList.remove('active')
 
     if (window.scrollY > 80) {
-        document.querySelector('.header .header-2').classList.add('active');
+        document.querySelector('.header .header-2').classList.add('active')
     }else {
-        document.querySelector('.header .header-2').classList.remove('active');
+        document.querySelector('.header .header-2').classList.remove('active')
     }
 }
 
-let loginForm = document.querySelector('.login-form-container');
+let loginForm = document.querySelector('.login-form-container')
 
 document.querySelector('#login-btn').onclick = () => {
-    loginForm.classList.toggle('active');
+    loginForm.classList.toggle('active')
 }
 
 document.querySelector('#close-login-btn').onclick = () => {
-    loginForm.classList.remove('active');
+    loginForm.classList.remove('active')
 }
 
 window.onload = () => {
     if (window.scrollY > 80) {
-        document.querySelector('.header .header-2').classList.add('active');
+        document.querySelector('.header .header-2').classList.add('active')
     }else {
-        document.querySelector('.header .header-2').classList.remove('active');
+        document.querySelector('.header .header-2').classList.remove('active')
     }
 }
 
@@ -48,6 +48,34 @@ var swiper = new Swiper(".books-slider", {
         },
         1024: {
             slidesPerView: 3,
-        },
+        }
+    }
+})
+
+var swiper = new Swiper(".featured-slider", {
+    spaceBetween: 10,
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
     },
-});
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        450: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        1024: {
+            slidesPerView: 4,
+        }
+    }
+})
